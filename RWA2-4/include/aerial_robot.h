34968f735244a0d7fbac174acb7ca10366370b88
Sarah Wielgosz <swielgosz20@gmail.com>
2023-11-08 11:25:16 -0500

    Implementing AerialRobot class

---
+++ RWA2-4/include/aerial_robot.h
@@ -0,0 +1,88 @@
+#pragma once
+
+#include <iostream>
+#include <utility>
+#include <chrono>
+#include "mobile_robot.h"
+
+/**
+ * @brief Namespace for RWA2 assignment
+ *
+ */
+namespace RWA2 {
+/**
+ * @brief Class for the derived aerial robot from base class MobileRobot
+ *
+ */
+class AerialRobot : public MobileRobot {
+ private:
+  // ==================== attributes ====================
+
+  //<! wing status of robot
+  bool has_wings_;
+  //<! altitude of the robot
+  double altitude_{0.0};
+  //<! flying status of robot
+  bool is_flying_{false};
+
+// ==================== constructors ====================
+
+public:
+  /**
+   * @brief Construct a new AerialRobot object
+   */
+
+  // Default ctor
+  AerialRobot(): MobileRobot(), altitude_{0.0}, is_flying_{false} {}
+
+  /**
+   * @brief Construct a new AerialRobot object
+   * - first argument: x position
+   * - second argument: y position
+   * - third argument: orientation
+   * - fourth argument: has wings
+   */
+  AerialRobot(double x_position, double y_position, double orientation, std::string model, bool has_wings) 
+  : MobileRobot(x_position, y_position, orientation, model), 
+  has_wings_{has_wings},
+  altitude_{0.0},
+  is_flying_{false} {}
+
+
+  // ==================== methods ====================
+
+public:
+  /**
+   * @brief Move aerial robot 
+   *
+   * @param distance distance to increase altitude by
+   * @param angle angle to rotate aerial robot
+   */
+  virtual void move(double distance, double angle) override;
+  /**
+   * @brief Print base class and AerialRobot class attributes
+   */
+  virtual void print_status() override;
+
+protected:  
+  /**
+   * @brief Rotate aerial robot 
+   * 
+   * @param angle angle to rotate aerial robot by (in degrees)
+   */
+  virtual void rotate(double angle) override; 
+
+private:
+  /**
+   * @brief Aerial Robot takes off to given altitude
+   *
+   * @param altitude_ altitude to take off to
+   */
+  void take_off(double altitude_);
+  /**
+   * @brief Land the aerial robot
+   */
+  void land();
+
+}; // class MobileRobot
+} // namespace RWA2
---
+++ RWA2-4/src/aerial_robot.cpp
@@ -0,0 +1,91 @@
+#include "aerial_robot.h"
+// #include "battery.h"  INCLUDE THIS WHEN BATTERY CLASS IS MADE
+// #include "sensor.h"  INCLUDE THIS WHEN SENSOR CLASS IS MADE
+#include <thread>
+#include <chrono>
+
+
+// Implement take_off method
+void RWA2::AerialRobot::take_off(double altitude) {
+  if (!is_flying_) {
+
+    // flight time in milliseconds
+    int flight_time_ms{};
+    if (has_wings_) {
+       flight_time_ms = static_cast<int>(1000*altitude/3);
+    } else {
+      flight_time_ms = static_cast<int>(1000*altitude/1.5);
+    }
+
+    // program sleeps for flight duration
+    std::chrono::milliseconds duration(flight_time_ms);
+    std::this_thread::sleep_for(duration);
+
+    // fly to new altitude
+    altitude_ = altitude;
+  }
+}
+
+// Implement land method
+void RWA2::AerialRobot::land() {
+  if (is_flying_) {
+
+    // landing time in milliseconds
+    int land_time_ms{};
+    if (has_wings_) {
+      land_time_ms = static_cast<int>(1000*altitude_/4);
+    } else {
+      land_time_ms = static_cast<int>(1000*altitude_/2);
+    }
+
+    // program sleeps for flight duration
+    std::chrono::milliseconds duration(land_time_ms);
+    std::this_thread::sleep_for(duration);
+
+    // altitude is 0 after landing
+    altitude_ = 0.0;
+  }
+}
+
+// Implement rotate method
+void RWA2::AerialRobot::rotate(double angle) {
+  std::cout << "AerialRobot::"<<RWA2::MobileRobot::model_ <<" rotated " << angle << " degrees.\n"; // test
+}
+
+// Implement print status method
+void RWA2::AerialRobot::print_status() {
+  RWA2::MobileRobot::print_status();
+  std::cout << "================\n";
+  std::cout
+       << "Has wings: " << std::boolalpha << has_wings_ << ", Flying: " << std::boolalpha << is_flying_ << ", Altitude: " << altitude_ << "\n";
+}
+
+// // Implement move method
+// void RWA2::AerialRobot::move(double distance, double angle) {
+
+//   if (distance > 25) { // check that distance <= 25 m; if true, proceed
+//     std::cout << "Max distance value is 25 m.\n\n";
+//   } else {
+//     // required battery to take off to distance and land, where 2% of batter is consumed each m the robot moves
+//     double required_battery = 2*2*distance;
+
+//     // if batter does not have enough charge, land and charge
+//     if (battery_.get_current_charge() <required_battery) {
+//       // land the robot, which discharges battery during landing at 2% per m
+//       land();
+//       battery_.discharge(2*altitude_); 
+//       //charge the robot
+//       battery_.start_charging(); 
+//     } 
+
+//     // when charge is sufficient, move robot
+//     battery_.discharge(required_battery);
+//     sensors_.read_data(5); // NOT SURE IF THIS IS CORRECT WAY TO CALL
+//     take_off(distance);
+//     rotate(angle);
+//     land();
+//     std::cout << model_ << " reached an altitude of " << distance << " meters and then landed.\n";
+//     print_status();
+//   }
+// }
+
